adf3442e56ced1a552dcd3837e65735e315c53cc
chinki kumari singh <singhchinkikumari.com>
2022-06-03 15:53:06 +0530

    [chinki] Static Function for check Two equal String Funcction

---
+++ AlgorithmProblem/AlgorithmProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿
+    String str = "ABC";
+    int n = str.Length;
+    AlgorithmProblem.StaticProgram.permute(str, 0, n - 1);
+
+    String[] arr = { "contribute", "geeks", "ide", "practice" };
+    String x = "ide";
+    int result = AlgorithmProblem.StaticProgram.binarySearch(arr, x);
+
+    if (result == -1)
+        Console.WriteLine("Element not present");
+    else
+        Console.WriteLine("Element found at "
+                        + "index " + result);
---
+++ AlgorithmProblem/AlgorithmProblem/StaticProgram.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    internal class StaticProgram
+    {
+        public static void permute(String str, int l, int r)
+
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+        public static String swap(String a, int i, int j)
+
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+       
+
+        public static int binarySearch(String[] arr, String x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+
+                int res = x.CompareTo(arr[m]);
+                if (res == 0)
+                    return m;
+                if (res > 0)
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+
+            return -1;
+        }
+
+
+    }
+}
